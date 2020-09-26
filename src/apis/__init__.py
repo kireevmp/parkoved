99dfdb3741a76eefb0b8a9d38e0f9033b90d871e
Kireev Mikhail <kireevmp@ya.ru>
2020-09-26 20:42:54 +0700

    Add parks api

--- src/apis/__init__.py
+++ src/apis/__init__.py
@@ -2,3 +2,4 @@ from .auth import router as auth_router
 from .users import router as users_router
 from .services import router as services_router
 from .tickets import router as tickets_router
+from .parks import router as parks_router
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from .apis import users_router, auth_router, services_router, tickets_router
+from .apis import users_router, auth_router, services_router, tickets_router, parks_router
 
 app = FastAPI()
 
@@ -8,3 +8,4 @@ app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
 app.include_router(users_router, prefix="/api/users", tags=["Users"])
 app.include_router(services_router, prefix="/api/services", tags=["Services"])
 app.include_router(tickets_router, prefix="/api/tickets", tags=["Tickets"])
+app.include_router(parks_router, prefix="/api/parks", tags=["Parks"])
